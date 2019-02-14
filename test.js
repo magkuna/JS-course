325f60763c6815e399983030378855f23c21503a
Magda Kuna <mag.kun@wp.pl>
2019-02-14 16:20:29 +0100

    else if (13)

--- test.js
+++ test.js
@@ -1,9 +1,13 @@
-var youLikeMeat = false;
-var MyNum = 8;
+var myAge = 12;
 
-if ( youLikeMeat){
-    document.write("you like meat");
+if (myAge > 30){
+    document.write("You are over 30!");
 }
-else{
-    document.write("you hate meat");
+else if(myAge > 20){
+    document.write("You are over 20!");
+} 
+else if(myAge > 10){
+    document.write("You are over 10!");
+}   else {
+    document.write("You are not over 10!");
 }
