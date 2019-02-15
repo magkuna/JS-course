6f45a4ca8b755d4a0e549830cdccc93cbfa9b12f
Magda Kuna <mag.kun@wp.pl>
2019-02-15 14:33:16 +0100

    variable scope (21)

--- test.js
+++ test.js
@@ -1,7 +1,14 @@
-function getAvarage (a,b,c,d,e,f){
-    var average = (a+b+c+d+e+f)/6;
+function getAvarage (a,b){
+    var average = (a+b)/2; //local variable
     console.log(average);
     return average;
+
 }
-var myResults=getAverage (7,8,9,10,11,12);
-console.log("the average is" + myresults);
+var myResults=getAverage (7,11);//global variable
+
+function logResults (){
+   
+console.log("the average is" + myResults + "inside the function"); 
+}
+
+logResults();
