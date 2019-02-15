dfa0566617b6b4577547de867ca84e55b18b6ff8
Magda Kuna <mag.kun@wp.pl>
2019-02-15 13:43:59 +0100

    for loops (17)

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-var age = 5;
+var links = document.getElementsByTagName("a");
 
-while (age = 10) {
-    console.log ("Your age is less than 10");
-    age++;
+for (i = 1; i < links.length; i++) {
+    console.log ("this is link number" + i);
+
 }
-document.write ("you are now over 10");
+document.write ("all links now looped"); 
