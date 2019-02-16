bf0730d470125a5e6e08770860e480978b607721
Magda Kuna <mag.kun@wp.pl>
2019-02-16 10:42:32 +0100

    constructor function (30)

--- test.js
+++ test.js
@@ -1,14 +1,19 @@
-var myCar2 = {
-
-    maxSpeed: 70,
-    driver:"Net Ninja",
-    drive: function(speed,time){
-        console.log(speed*time);},
-
-    logDriver: function(){
-        console.log("driver name is " + this.driver);}
+var Car = function (maxSpeed, driver) {
+    
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function (speed, time){
+        console.log (speed * time);
     };
+    this.logDriver = function (){
+        console.log("driver name is " + this.driver);}
+    
+}
 
-myCar2.logDriver()
-console.log(myCar2.maxSpeed);
-myCar2.drive (50, 3);
+var myCar = new Car (70, "Ninja Man");
+var myCar2 = new Car (40, "humpty");
+var myCar3 = new Car (10, "Shaun");
+var myCar4 = new Car (90, "James ");
+
+myCar.drive(30,5);
+myCar3.logDriver(); 
